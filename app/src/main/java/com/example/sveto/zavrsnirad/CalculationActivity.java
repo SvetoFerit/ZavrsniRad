4fdc8febe9851265f4d43b810da84a640648cb80
SvetoFerit <38116352+SvetoFerit@users.noreply.github.com>
2018-08-25 16:30:21 +0200

    add app bar,callback method,drawer....

--- app/src/main/java/com/example/sveto/zavrsnirad/CalculationActivity.java
+++ app/src/main/java/com/example/sveto/zavrsnirad/CalculationActivity.java
@@ -56,6 +56,14 @@ public class CalculationActivity extends AppCompatActivity {
     private View navHeader;
     private TextView txtName, txtWebsite;
 
+    float weight;
+    float height;
+    int years;
+
+    float recommended_calories;
+    float recommended_kilometers;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,6 +79,7 @@ public class CalculationActivity extends AppCompatActivity {
         loadNavHeader();
         setUpNavigationView();
 
+        setParameters();
         calculation();
     }
 
@@ -87,21 +96,25 @@ public class CalculationActivity extends AppCompatActivity {
 
                 switch (menuItem.getItemId()) {
                     case R.id.nav_sync:
-                        final ProgressDialog progressBar = new ProgressDialog(CalculationActivity.this);
-                        progressBar.setMessage("Syncing....");
-                        progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                        progressBar.setProgress(3);
-                        progressBar.show();
-                        final Handler handler = new Handler();
-                        handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                progressBar.dismiss();
-                                Toast.makeText(CalculationActivity.this, "You are successfully synced", Toast.LENGTH_SHORT).show();
-                                drawerLayout.closeDrawers();
-                                syncFitbit();
-                            }
-                        }, 3000);
+                        if (FitbitUtils.connectedToInternet(CalculationActivity.this)) {
+                            final ProgressDialog progressBar = new ProgressDialog(CalculationActivity.this);
+                            progressBar.setMessage("Syncing....");
+                            progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                            progressBar.setProgress(3);
+                            progressBar.show();
+                            final Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.dismiss();
+                                    Toast.makeText(CalculationActivity.this, "You are successfully synced", Toast.LENGTH_SHORT).show();
+                                    drawerLayout.closeDrawers();
+                                    syncFitbit();
+                                }
+                            }, 3000);
+                        } else {
+                            Toast.makeText(CalculationActivity.this, "No internet connection", Toast.LENGTH_SHORT).show();
+                        }
 
                         break;
                     case R.id.nav_set_weight_and_height:
@@ -142,6 +155,15 @@ public class CalculationActivity extends AppCompatActivity {
         actionBarDrawerToggle.syncState();
     }
 
+    public void setParameters() {
+
+        weight = PreferenceUtils.getWeight(this);
+        height = PreferenceUtils.getHeight(this);
+        years = PreferenceUtils.getYears(this);
+        tvCurrentSteps.setText(String.valueOf(getIntent().getIntExtra(ConnectFitbitActivity.EXTRA_STEPS, 0)));
+        tvCurrentCalories.setText(String.valueOf(getIntent().getIntExtra(ConnectFitbitActivity.EXTRA_CALORIES, 0)));
+        tvCurrentKilometers.setText(String.format(Locale.getDefault(), "%.3f", 0.414f * (height / 100000) * getIntent().getIntExtra(ConnectFitbitActivity.EXTRA_STEPS, 0)));
+    }
 
     public void calculation() {
 
@@ -149,9 +171,6 @@ public class CalculationActivity extends AppCompatActivity {
         alert.setTitle("Pažnja!");
         alert.setPositiveButton("OK", null);
 
-        float weight = PreferenceUtils.getWeight(this);
-        float height = PreferenceUtils.getHeight(this);
-        int years = PreferenceUtils.getYears(this);
         float BMI = weight / (((height) / 100) * ((height) / 100));
 
         if (BMI > 25) {
@@ -159,35 +178,31 @@ public class CalculationActivity extends AppCompatActivity {
             alert.setMessage(R.string.pazi_na_tezinu);
             alert.show();
 
-            int recommended_calories = (int) (10 * weight + 6.25 * height - 5 * years + 5 + 700);
-            float recommended_kilometers = 0.414f * (height / 100000) * 15000;
-            tvRecommendedCalories.setText(String.valueOf(recommended_calories));
+            recommended_calories = 10 * weight + 6.25f * height - 5 * years + 5 + 700;
+            recommended_kilometers = 0.414f * (height / 100000) * 15000;
+            tvRecommendedCalories.setText(String.format(Locale.getDefault(), "%.0f", recommended_calories));
             tvRecommendedSteps.setText("15000");
             tvRecommendedKilometers.setText(String.format(Locale.getDefault(), "%.3f", recommended_kilometers));
         } else if (BMI < 18.5) {
             alert.setMessage(R.string.pazi_na_tezinu);
             alert.show();
 
-            int recommended_calories = (int) (10 * weight + 6.25 * height - 5 * years + 5);
-            float recommended_kilometers = 0.414f * (height / 100000) * 20000;
-            tvRecommendedCalories.setText(String.valueOf(recommended_calories));
+            recommended_calories = 10 * weight + 6.25f * height - 5 * years + 5 - 400;
+            recommended_kilometers = 0.414f * (height / 100000) * 2000;
+            tvRecommendedCalories.setText(String.format(Locale.getDefault(), "%.0f", recommended_calories));
             tvRecommendedSteps.setText("2000");
             tvRecommendedKilometers.setText(String.format(Locale.getDefault(), "%.3f", recommended_kilometers));
         } else {
             alert.setMessage(R.string.tezine_je_dobra);
             alert.show();
 
-            int recommended_calories = (int) (10 * weight + 6.25 * height - 5 * years + 5 + 400);
-            float recommended_kilometers = 0.414f * (height / 100000) * 10000;
+            recommended_calories = 10 * weight + 6.25f * height - 5 * years + 5 + 400;
+            recommended_kilometers = 0.414f * (height / 100000) * 10000;
 
-            tvRecommendedCalories.setText(String.valueOf(recommended_calories));
+            tvRecommendedCalories.setText(String.format(Locale.getDefault(), "%.0f", recommended_calories));
             tvRecommendedSteps.setText("10000");
             tvRecommendedKilometers.setText(String.format(Locale.getDefault(), "%.3f", recommended_kilometers));
         }
-
-        tvCurrentSteps.setText(String.valueOf(getIntent().getIntExtra(ConnectFitbitActivity.EXTRA_STEPS, 0)));
-        tvCurrentCalories.setText(String.valueOf(getIntent().getIntExtra(ConnectFitbitActivity.EXTRA_CALORIES, 0)));
-        tvCurrentKilometers.setText(String.format(Locale.getDefault(), "%.3f", 0.414f * (height / 100000) * getIntent().getIntExtra(ConnectFitbitActivity.EXTRA_STEPS, 0)));
     }
 
     public void syncFitbit() {
@@ -200,6 +215,19 @@ public class CalculationActivity extends AppCompatActivity {
                 tvCurrentSteps.setText(String.valueOf(FitbitParameters.STEPS));
                 tvCurrentKilometers.setText(String.format(Locale.getDefault(), "%.3f", 0.414f * (height / 100000) * FitbitParameters.STEPS));
 
+                AlertDialog.Builder alert = new AlertDialog.Builder(CalculationActivity.this);
+                alert.setTitle("Upozorenje!");
+                alert.setPositiveButton("OK", null);
+                if (recommended_calories != FitbitParameters.CALORIES && (recommended_kilometers != 0.414f * (height / 100000) * FitbitParameters.STEPS)) {
+                    alert.setMessage("Preporučen broj potrošenih kalorija i pređenih kilometara nije ostvaren!");
+                    alert.show();
+                } else if (recommended_kilometers != 0.414f * (height / 100000) * FitbitParameters.STEPS) {
+                    alert.setMessage("Preporučeni broj kilometara i koraka nije ostvaren!");
+                    alert.show();
+                } else {
+                    alert.setMessage("Preporučen broj potrošenih kalorija nije ostvaren");
+                    alert.show();
+                }
             }
         });
 
